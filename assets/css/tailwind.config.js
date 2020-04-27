f51b3f1e0547213315e432ecf6b3c60c87c21811
Washington Zhao <washingz@usc.edu>
2020-04-27 10:41:32 -0700

    first three sections of site

---
+++ assets/css/tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  important: true,
+  theme: {
+    maxWidth:{
+      '1/10': '10%',
+      '1/4': '25%',
+      '1/3': '33%',
+      '1/2': '50%',
+      '3/4': '75%',
+    },
+    minHeight:{
+      '3/4': '75%'
+    },
+    fontFamily:{
+      'Neue': 'Neue Haas Grotesk Display Pro',
+      'Aktiv': 'Aktiv Grotesk',
+      'Messina': 'Messina, Serif'
+    },
+
+    extend: {
+      colors:{
+        SparkYellow: '#FDBE13',
+        pale: '#FFF9E9',
+        italicGray: '#7A7A7A',
+        OpacitySpark: 'rgb(253,190,19,0.2)',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
