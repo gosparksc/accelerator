121129bdb6b492b14ff3aa695eb4e989a9956718
Washington Zhao <washingz@usc.edu>
2020-04-28 19:31:05 -0700

    first desktop version

--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
       '1/10': '10%',
       '1/4': '25%',
       '1/3': '33%',
+      '1/25': '40%',
       '1/2': '50%',
       '3/4': '75%',
     },
@@ -14,7 +15,8 @@ module.exports = {
     fontFamily:{
       'Neue': 'Neue Haas Grotesk Display Pro',
       'Aktiv': 'Aktiv Grotesk',
-      'Messina': 'Messina, Serif'
+      'Messina': 'Messina, Serif',
+      'DM': 'DM, Sans Serif'
     },
 
     extend: {
@@ -23,6 +25,9 @@ module.exports = {
         pale: '#FFF9E9',
         italicGray: '#7A7A7A',
         OpacitySpark: 'rgb(253,190,19,0.2)',
+        form: '#FFEDBE',
+        formBox: '#FAE2A3',
+        company: '#4C585C',
       }
     },
   },
